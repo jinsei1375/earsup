72891cfd48ca1110adb2995a1a7971fd31012ffb
Shinsei <murakami.shinsei.zn@gmail.com>
2025-07-28 09:20:29 +0900

    登録用テーブルuser_sentences追加

--- types/index.ts
+++ types/index.ts
@@ -38,6 +38,15 @@ export interface Answer {
   nickname?: string;
 }
 
+export interface UserSentence {
+  id: string;
+  user_id: string;
+  text: string;
+  translation: string;
+  created_at: string;
+  updated_at?: string;
+}
+
 export interface Buzz {
   id: string;
   room_id: string;
